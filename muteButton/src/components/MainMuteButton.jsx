52162e87d8b370e56ab682ed72c9e9f57c69a1e5
Luis Manzo <manzosk3@gmail.com>
2022-07-19 20:05:54 -0500

    original solution

--- muteButton/src/components/MainMuteButton.jsx
+++ muteButton/src/components/MainMuteButton.jsx
@@ -1,17 +1,6 @@
-import On from '../../icons/on.jsx'
-import Off from '../../icons/off.jsx'
-
 function MyButton(props) {
-    
-    // console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>',props.path)
-    // console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>',props)
     return (
-        <button onClick={props.handleClick}>
-
-            {/* {props.path == props.path['on'] ? setImagePath(props.path['off']): console.log('>>>>>>>>>>>>>>>')} */}
-            {props.isMuted ? <Off /> : <On />}
-            
-        </button>
+        <img onClick={props.handleClick} src={props.imagePath} />
     )
 }
 export default MyButton
